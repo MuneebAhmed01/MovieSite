68144593fc823f8d1ac6694c9c3d519c295ddb68
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-11-19 22:56:06 +0500

    ..

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { useAppContext } from "../context/AppContext";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
+  const navigate = useNavigate();
+
+  const { favoriteMovies } = useAppContext();
+
+  return (
+    <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
+      <Link to="/" className="max-md:flex-1">
+        <img src={assets.logo} alt="logo" className="w-36 h-auto" />
+      </Link>
+
+      
+
+      <div className="flex items-center gap-8">
+        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
+        {!user ? (
+          <button
+            onClick={openSignIn}
+            className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<TicketPlus width={15} />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
+      </div>
+
+      <MenuIcon
+        onClick={() => setIsOpen(!isOpen)}
+        className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
