0378fb691b5eb3a550792b7435b1aae95eb1fe3c
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-12-04 21:19:29 +0500

    .

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -20,7 +20,32 @@ const Footer = () => {
             <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
           </div>
         </div>
-
+        <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
+          <div>
+            <h2 className="font-semibold mb-5">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-semibold mb-5">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+250-784-652-570</p>
+              <p>elyseniyibizi@gmail.com</p>
+            </div>
+          </div>
+        </div>
       </div>
       <p className="pt-4 text-center text-sm pb-5">
         Copyright {new Date().getFullYear()} © ElyséeDev. All Right Reserved.
