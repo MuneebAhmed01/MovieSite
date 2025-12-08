7310f98b38fb9d8621d22aa943461653257a33a5
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-12-08 15:37:59 +0500

    .

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [shows, setShows] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+
+  const image_base_url = import.meta.env.VITE_TMDB_IMAGE_BASE_URL;
+
+  const { user } = useUser();
+  const { getToken } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => useContext(AppContext);
